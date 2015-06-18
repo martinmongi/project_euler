46dd79066c84e001ac8ac31d720cb1b99ae3530c
Martín Mongi Badía <martinmongi@gmail.com>
2015-06-17 23:42:48 -0300

    50 done

--- 50.py
+++ 50.py
@@ -4,3 +4,24 @@ from lib import cribe
 
 c = cribe(10**6)
 
+primes = []
+
+for i in range(len(c)):
+	if c[i]:
+		primes.append(i)
+
+
+l = 0
+
+while l < len(primes):
+	l = l + 1
+	i = 0
+	found = False
+	while i + l < len(primes) and not found:
+		a = sum(primes[i:i+l])
+		if a < len(c):
+			if c[a]:
+				found = True
+				print(a, l, primes[i], primes[i+l-1])
+		i = i + 1
+
