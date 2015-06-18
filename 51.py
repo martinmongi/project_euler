34e73ed94dbc4adb9f23460f9866996be5e14aac
Martín Mongi Badía <martinmongi@gmail.com>
2015-06-18 00:41:26 -0300

    51

---
+++ 51.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from lib import cribe
+import itertools
+
+max_i = 10**6
+c = cribe(max_i)
+
+max_count = 0
+for i in range(56003, max_i, 2):
+	#print(i)
+	#print("============")
+	##if c[i]:
+	s = str(i)
+	for mask in itertools.product(range(2), repeat=len(s)):
+		stars = []
+		#print(mask)
+		for j in range(len(mask)):
+
+			if mask[j] == 1:
+				stars.append(j)
+		#print(stars)
+
+		if len(stars) > 0:
+			if mask[0] == 1:
+				start = ord('1')
+			else:
+				start = ord('0')
+			count = 0
+			for digit in range(start, ord('9') + 1):
+				n = list(s)
+				for j in stars:
+					n[j] = chr(digit)
+				n = ''.join(n)
+				if c[int(n)]:
+					count = count + 1
+			if count > max_count:
+				max_count = count
+				print(i, count, mask)
--- lib.py
+++ lib.py
@@ -7,3 +7,14 @@ def cribe(n):
 				c[j] = False
 
 	return c
+
+def prime(n):
+	if n < 2:
+		return False
+	elif n == 2:
+		return True
+	for i in range(3, int(n**0.5) + 1, 2):
+		if n % i == 0:
+			return False
+	return True
+
